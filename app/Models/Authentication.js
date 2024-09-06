4a81d7ea69c48065f60450d6c25f6f4aa9e3a5a8
Brijmohan-github <bmk302006@gmail.com>
2024-09-06 11:28:58 +0530

    Delete app/Models/Authentication.js

--- app/Models/Authentication.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AuthenticationSchema = new mongoose.Schema({
-  token: String,
-  productFamily: Number,
-  gameId: String
-});
-
-module.exports = mongoose.model('Authentication', AuthenticationSchema);
