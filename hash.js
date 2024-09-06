16a8648def94b2c44570bb9e2926e9d78c847e75
Brijmohan-github <bmk302006@gmail.com>
2024-09-06 13:07:36 +0530

    Update hash.js

--- hash.js
+++ hash.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
+const requestBody = JSON.stringify({ Token: '123xyz', GameId: '2', ProductFamily: '1' });
 const passKey = 'Pwd123';
-const requestBody = JSON.stringify({ Token: '123xyz' });
-const hash = crypto.createHash('md5').update(requestBody + passKey).digest('hex').toUpperCase();
-console.log(hash); // Use this value in the hash parameter
+const concatenatedString = requestBody + passKey;
+const hash = crypto.createHash('md5').update(concatenatedString).digest('hex').toUpperCase();
+console.log('Generated Hash:', hash);
