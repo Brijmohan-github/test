d4a9a97c52449c5371ef8825bfa0d5db985045a2
Brijmohan-github <bmk302006@gmail.com>
2024-09-06 13:09:53 +0530

    Update HashUtil.js

--- app/Utils/HashUtil.js
+++ app/Utils/HashUtil.js
@@ -1,29 +1,21 @@
+// app/Utils/HashUtil.js
+
+'use strict';
+
 const crypto = require('crypto');
 
-/**
- * Generate MD5 hash
- * @param {string} requestBody - The body of the request
- * @param {string} passKey - The PassKey for hashing
- * @return {string} - The generated MD5 hash
- */
-function generateMD5Hash(requestBody, passKey) {
-    const concatenatedString = requestBody + passKey;
-    return crypto.createHash('md5').update(concatenatedString).digest('hex').toUpperCase();
+class HashUtil {
+    // Function to encode and generate MD5 hash
+    static encode(requestBody, passKey) {
+        const concatenatedString = JSON.stringify(requestBody) + passKey;
+        return crypto.createHash('md5').update(concatenatedString).digest('hex').toUpperCase();
+    }
+
+    // Function to validate the hash
+    static decode(requestBody, receivedHash, passKey) {
+        const generatedHash = this.encode(requestBody, passKey);
+        return generatedHash === receivedHash;
+    }
 }
 
-/**
- * Validate the MD5 hash
- * @param {string} requestBody - The body of the request
- * @param {string} passKey - The PassKey for hashing
- * @param {string} receivedHash - The hash received in the request
- * @return {boolean} - True if the hash is valid, otherwise false
- */
-function validateMD5Hash(requestBody, passKey, receivedHash) {
-    const expectedHash = generateMD5Hash(requestBody, passKey);
-    return expectedHash === receivedHash;
-}
-
-module.exports = {
-    generateMD5Hash,
-    validateMD5Hash
-};
+module.exports = HashUtil;
